a489905ab490c09c6833d40098294b1b14d734b0
mwerofaith5-collab <mwerofaith5@gmail.com>
2025-12-05 10:03:20 +0300

    Add files via upload

---
+++ LIST WITH DICTIONARY.py
@@ -0,0 +1,25 @@
+
+class Node:
+    def init(self, data):
+        self.data = data   
+        self.next = None   
+class LinkedList:
+    def init(self):
+        self.head = None
+
+    def insert(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data)
+            current = current.next
+            students = LinkedList()
